bb066fdfdeeeebee25479a4853ad7a8739a5b6e3
khamro066 <normamatov066@gmail.com>
2025-09-17 02:28:53 +0500

    Create hw1.py

---
+++ lesson-1/homework/hw1.py
@@ -0,0 +1,32 @@
+1.Given a side of square. Find its perimeter and area.
+
+print (side)
+print(perimeter)
+print(area)
+
+side=7
+perimeter=4* side
+area=side*side
+
+2.Given diameter of circle. Find its length.
+
+import math
+d = float(input())
+print(L)
+
+3.Given two numbers a and b. Find their mean.  
+a = float(input())
+b = float(input())
+
+mean = (a + b) / 2
+print(mean)
+
+4.Given two numbers a and b. Find their sum, product and square of each number.
+
+a = float(input())
+b = float(input())
+
+print(a + b)   
+print(a * b)   
+print(a ** 2)  
+print(b ** 2) 
